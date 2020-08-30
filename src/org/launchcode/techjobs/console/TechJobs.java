f4dfaf31de4a0e61af4cd5247dd941ae558826f6
KBarr <ksamuels6@yahoo.com>
2020-08-30 17:17:36 -0500

    Assignment 1 completed

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -62,7 +62,7 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -112,10 +112,10 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("*****");
+
         if (someJobs.isEmpty()) {
             System.out.println("There are no results");
-            System.out.println("*****");
+
         } else {
         for (int i = 0; i < someJobs.size(); i++) {
             for (Map.Entry<String, String> someJob : someJobs.get(i).entrySet()) {
